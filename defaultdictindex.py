08776e7483f17cb7b4f22606fd0a4d862d9d0098
SudhaAmarnath <sudha04.a@gmail.com>
2020-06-11 00:46:36 -0700

    algorithms16

--- defaultdictindex.py
+++ defaultdictindex.py
@@ -18,6 +18,16 @@ Explanation
 'a' appeared  times in positions ,  and .
 'b' appeared  times in positions  and .
 In the sample problem, if 'c' also appeared in word group , you would print .
+from collections import defaultdict
+d = defaultdict(list)
+d['python'].append("awesome")
+d['something-else'].append("not relevant")
+d['python'].append("language")
+for i in d.items():
+    print(i)
+
+result
+
 '''
 from collections import defaultdict
 d = defaultdict(list)
@@ -28,3 +38,28 @@ for i in range(1, n + 1):
 
 for i in range(m):
     print(' '.join(d[input()]) or -1)
+
+'''
+('a', [0, 1, 6])
+('b', [2, 3])
+('c', [10, 11, 12])
+('d', [9, 13])
+('e', [4, 7, 14])
+('f', [5, 8, 15])
+
+from collections import defaultdict
+
+def duplicatepos(s):
+    d = defaultdict(list)
+    for i,item in enumerate(s):
+        d[item].append(i)
+    return ((k,v) for k,v in d.items()
+                            if len(v)>1)
+
+for j in sorted(duplicatepos('aabbefaefdcccdefs')):
+    print(j)
+
+#or
+
+
+'''
