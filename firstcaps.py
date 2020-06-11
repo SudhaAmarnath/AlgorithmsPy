85718dbab11f2cab87ede6363f562b21472240aa
SudhaAmarnath <sudha04.a@gmail.com>
2020-06-10 20:21:48 -0700

    algorithms14

--- firstcaps.py
+++ firstcaps.py
@@ -3,7 +3,33 @@ def firstletterincaps(s):
     return s.title()
 print(firstletterincaps('hello world'))
 '''
+
+#or
+
 def firstletterincaps(s):
     lst = [word[0].upper() + word[1:] for word in s.split()]
     return " ".join(lst)
 print(firstletterincaps('hello world'))
+
+#or
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def solve(s):
+    for x in s.split():
+        s = s.replace(x, x.capitalize())
+    return s
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    s = input()
+    result = solve(s)
+    fptr.write(result + '\n')
+    fptr.close()
