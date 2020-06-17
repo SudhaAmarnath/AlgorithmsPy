9a0c62793f8c16bf552bcb648d33fcde634b8a2f
SudhaAmarnath <sudha04.a@gmail.com>
2020-06-16 22:28:39 -0700

    algorithms21

--- floatingpointregex.py
+++ floatingpointregex.py
@@ -1,5 +1,5 @@
 '''
-Input 
+Input
 4
 4.0O0
 -1.00
@@ -16,3 +16,19 @@ from re import match, compile
 patt = compile('^[-+]?[0-9]*\.[0-9]+$')
 for _ in range(int(input())):
     print(bool(patt.match(input())))
+
+#re.split()
+'''
+Input 
+100,000,000.000
+
+Output 
+100
+000
+000
+000
+'''
+regex_pattern = r"[,.]+"	# Do not delete 'r'.
+
+import re
+print("\n".join(re.split(regex_pattern, input())))
