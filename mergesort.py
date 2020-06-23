63b8da0d88145b5d719733844ad49be6cb99e1bf
SudhaAmarnath <sudha04.a@gmail.com>
2020-06-23 00:23:59 -0700

    algorithms32

--- mergesort.py
+++ mergesort.py
@@ -1,9 +1,35 @@
+def merge(arr):
+    if len(arr)<= 1:
+        return arr
+    else:
+        mid = len(arr)//2
+        left = merge(arr[:mid])
+        right = merge(arr[mid:])
+        return mergesort(left,right)
+
+def mergesort(left,right):
+    result = []
+    i,j = 0,0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result += left[i:]
+    result += right[j:]
+    return result
+print(merge([13,7,1,4,5,21]))
+
+
+#merge two lists
 def mergesort(list1, list2):
     list3 = []
     a = 0
     b = 0
     while a<len(list1) and b<len(list2):
-        if list1[a] < list2[b]:
+        if list1[a] <= list2[b]:
             list3.append(list1[a])
             a+=1
         else:
@@ -14,5 +40,5 @@ def mergesort(list1, list2):
     else:
         list3.extend(list1[a:])
     return list3
-print(mergesort([2,5,6,7,1],[9,4,2,7,8]))
+print(mergesort([2,5,6,7],[1,3,8,10]))
 
