99f60d34306b2247cfa43a81cb9bcba3181c05fe
SudhaAmarnath <sudha04.a@gmail.com>
2020-06-22 18:07:31 -0700

    algorithms31

--- heapsort.py
+++ heapsort.py
@@ -1,5 +1,15 @@
 import heapq
 def heapsort(array):
+    h = array.copy()
+    heapq.heapify(h)
+    return [heapq.heappop(h) for _ in range(len(array))]
+print(heapsort([5,3,4,6,1,7,2]))
+
+
+#or
+
+import heapq
+def heapsort(array):
     heap = []
     for element in array:
         heapq.heappush(heap, element)
