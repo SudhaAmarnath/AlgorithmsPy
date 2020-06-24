36af88b8e11f11fee960b07e4da0fe55821cbe82
SudhaAmarnath <sudha04.a@gmail.com>
2020-06-24 16:42:37 -0700

    algorithms38

--- countbits.py
+++ countbits.py
@@ -1,3 +1,22 @@
+'''
+Example 1:
+
+Input: 00000000000000000000000000001011
+Output: 3
+Explanation: The input binary string 00000000000000000000000000001011 has a total of three '1' bits.
+Example 2:
+
+Input: 00000000000000000000000010000000
+Output: 1
+Explanation: The input binary string 00000000000000000000000010000000 has a total of one '1' bit.
+Example 3:
+
+Input: 11111111111111111111111111111101
+Output: 31
+Explanation: The input binary string 11111111111111111111111111111101 has a total of thirty one '1' bits.
+'''
+
+
 class countbits:
     def bits(self, num: int) -> int:
         arr = [0]
