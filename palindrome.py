226f2f3cb06088b352eaae558e5f54a70a5f3501
SudhaAmarnath <sudha04.a@gmail.com>
2020-06-25 23:44:36 -0700

    leetalgorithms

--- palindrome.py
+++ palindrome.py
@@ -13,3 +13,24 @@ print(is_palindrome("sudha"))
 #using any()
 s = "racecars"
 print(any(x == x[::-1] for x in s.split())
+
+#Valid palindrome leetcode
+
+
+class Solution:
+  # O(n)|O(1)
+  def isPalindrome(self, s: str) -> bool:
+    l = 0
+    r = len(s) - 1
+    while l < r:
+      if not s[l].isalnum():
+        l += 1
+      elif not s[r].isalnum():
+        r -= 1
+      else:
+        if not s[l].lower() == s[r].lower():
+          return False
+        else:
+          l += 1
+          r -= 1
+    return True
