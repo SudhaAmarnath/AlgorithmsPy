010991b7893d079cda2c622aa9a64fd5e9407936
SudhaAmarnath <sudha04.a@gmail.com>
2020-06-26 00:07:24 -0700

    leetalgorithms

--- palindrome.py
+++ palindrome.py
@@ -15,6 +15,20 @@ s = "racecars"
 print(any(x == x[::-1] for x in s.split())
 
 #Valid palindrome leetcode
+'''
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+Example 1:
+
+Input: "A man, a plan, a canal: Panama"
+Output: true
+Example 2:
+
+Input: "race a car"
+Output: false
+'''
 
 
 class Solution:
@@ -34,3 +48,17 @@ class Solution:
           l += 1
           r -= 1
     return True
+
+#or
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+      s = [i for i in s.lower() if s.isalnum()]
+      return s == s[::-1]
+#or
+import re
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = re.sub('[^0-9A-Za-z]+','',s)
+        s = s.lower()
+        return s==s[::-1]
