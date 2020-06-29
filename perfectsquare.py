e554b3473c3657b101d22dff233c3e34d6094ae0
SudhaAmarnath <sudha04.a@gmail.com>
2020-06-29 12:39:23 -0700

    leetalgorithms

--- perfectsquare.py
+++ perfectsquare.py
@@ -3,3 +3,19 @@ def is_square(num: int) -> bool:
     return num % math.sqrt(num) == 0
 print(is_square(2)) #False
 print(is_square(16)) #True
+
+#or
+
+def perfecsquare(n):
+    x = n // 2
+    y = set([x])
+    while x * x != n:
+        x = (x + (n // x)) // 2
+        if x in y:
+            return False
+        y.add(x)
+    return True
+
+print(perfecsquare(12))
+print(perfecsquare(16))
+print(perfecsquare(64))
