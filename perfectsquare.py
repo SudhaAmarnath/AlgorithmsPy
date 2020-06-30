e42f1dcf33ff886c1cd00dbdc25f63f4abdc0f00
SudhaAmarnath <sudha04.a@gmail.com>
2020-06-29 20:27:21 -0700

    leetalgorithms

--- perfectsquare.py
+++ perfectsquare.py
@@ -57,3 +57,12 @@ def numSquares(target):
     return level
 print(numSquares(12))
 '''
+
+#or
+class Solution(object):
+    num = [0]
+    def numSquares(self, n):
+        dp = self.num
+        while len(dp) <= n:
+            dp += min(dp[-i*i] for i in range(1, int(len(dp)**0.5+1))) + 1,
+        return dp[n]
