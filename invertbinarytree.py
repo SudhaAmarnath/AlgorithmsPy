d51f575c3be41c193a1b44a24692f30d457b4d3c
SudhaAmarnath <sudha04.a@gmail.com>
2020-07-01 20:54:14 -0700

    leetalgorithms

--- invertbinarytree.py
+++ invertbinarytree.py
@@ -34,3 +34,15 @@ class Solution:
     def invertTree(self, r: TreeNode) -> TreeNode:
         f(r)
         return r
+
+#or
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if root is None:
+            return None
+        r = self.invertTree(root.right)
+        l = self.invertTree(root.left)
+        root.right = l
+        root.left = r
+
+        return root
