4019a0280cfa97bcae8a21286fa78fbf8cdb48a4
SudhaAmarnath <sudha04.a@gmail.com>
2020-07-01 19:12:01 -0700

    leetalgorithms

---
+++ invertbinarytree.py
@@ -0,0 +1,36 @@
+'''
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+def f(r):
+    if r==None:return
+    r.left,r.right=r.right,r.left
+    f(r.left)
+    f(r.right)
+class Solution:
+    def invertTree(self, r: TreeNode) -> TreeNode:
+        f(r)
+        return r
