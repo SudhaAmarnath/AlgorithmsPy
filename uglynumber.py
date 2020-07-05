3a47779f6ec670fcaa25c079f7e9437bf53ddcc6
SudhaAmarnath <sudha04.a@gmail.com>
2020-07-05 15:00:12 -0700

    leetalgorithms

--- uglynumber.py
+++ uglynumber.py
@@ -1,3 +1,25 @@
+'''
+Write a program to check whether a given number is an ugly number.
+
+Ugly numbers are positive numbers whose prime factors only include 2, 3, 5.
+
+Example 1:
+
+Input: 6
+Output: true
+Explanation: 6 = 2 × 3
+Example 2:
+
+Input: 8
+Output: true
+Explanation: 8 = 2 × 2 × 2
+Example 3:
+
+Input: 14
+Output: false
+Explanation: 14 is not ugly since it includes another prime factor 7.
+'''
+
 class nthuglynumber:
     def uglynumber(self, n: int) -> int:
 
@@ -13,3 +35,23 @@ class nthuglynumber:
                 p5 += 1
         return dp[-1]
 print(nthuglynumber().uglynumber(20))
+
+#or
+
+class Solution:
+    def isUgly(self, num: int) -> bool:
+
+        if num <= 0:
+            return False
+
+        while num > 0:
+            if num == 2 or num == 3 or num == 5 or num == 1:
+                return True
+            elif num % 2 == 0:
+                num /= 2
+            elif num % 3 == 0:
+                num /= 3
+            elif num % 5 == 0:
+                num /= 5
+            else:
+                return False
