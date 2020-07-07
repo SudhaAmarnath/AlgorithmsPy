8ee1dea5510a8c020d7442b903017f8e9278555d
SudhaAmarnath <sudha04.a@gmail.com>
2020-07-06 20:27:40 -0700

    bitwise problems

--- binary/sumofintergers.py
+++ binary/sumofintergers.py
@@ -1,6 +1,22 @@
-def solvemefirst(a,b):
-    return a+b
-num1 = int(input())
-num2 = int(input())
-res = solvemefirst(num1,num2)
-print(res)
+'''
+Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+
+Example 1:
+
+Input: a = 1, b = 2
+Output: 3
+Example 2:
+
+Input: a = -2, b = 3
+Output: 1
+'''
+
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        def myadd(c, d):
+            while d != 0:
+                c, d = c^d, (c&d)<<1
+            return c
+        if a < 0 and myadd(~a, 1) <= b or b < 0 and myadd(~b, 1) <= a:
+            return ~myadd(myadd(~a, ~b), 1)
+        return myadd(a, b)
