da8e69ff0f3f0f71c52d3ed7a54cbd101786a710
SudhaAmarnath <sudha04.a@gmail.com>
2020-07-06 20:26:26 -0700

    array based problems

--- array/missingnumber.py
+++ array/missingnumber.py
@@ -15,3 +15,16 @@ class missingnumber:
         formula = n*(n+1)//2
         return formula - total
 print(missingnumber().missing([1,2,0,4]))
+
+# or using XOR - effient way
+def missing(arr):
+    a = arr[0]
+    b = 1
+    for i in range(len(arr)-1):
+        a = a^arr[i]
+    for i in range(1,len(arr)):
+        b = b^i
+
+        return a^b #XOR between natural numbers and arr
+
+print(missing([1,2,3,5,6]))
