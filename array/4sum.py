6172595a6c04a7a095e220b3da72d712ee8e4618
SudhaAmarnath <sudha04.a@gmail.com>
2020-07-08 23:17:53 -0700

    array four sum

--- array/4sum.py
+++ array/4sum.py
@@ -50,7 +50,41 @@ class Solution:
                     else:
                         k += 1
         return result
+
+
+#or using itertools combinations
+import itertools
+list=[1, 0, -1, 0, -2, 2]
+#s = set(list)
+#for i in itertools.combinations(s,3):
+for i in itertools.combinations(list, 4):
+    if i[0]+i[1]+i[2]+i[3] == 0:
+        print(i)
+
+
+
 #4sum ii
+'''
+Given four lists A, B, C, D of integer values, compute how many tuples (i, j, k, l) there are such that A[i] + B[j] + C[k] + D[l] is zero.
+
+To make problem a bit easier, all A, B, C, D have same length of N where 0 ≤ N ≤ 500. All integers are in the range of -228 to 228 - 1 and the result is guaranteed to be at most 231 - 1.
+
+Example:
+
+Input:
+A = [ 1, 2]
+B = [-2,-1]
+C = [-1, 2]
+D = [ 0, 2]
+
+Output:
+2
+
+Explanation:
+The two tuples are:
+1. (0, 0, 0, 1) -> A[0] + B[0] + C[0] + D[1] = 1 + (-2) + (-1) + 2 = 0
+2. (1, 1, 0, 0) -> A[1] + B[1] + C[0] + D[0] = 2 + (-1) + (-1) + 0 = 0
+'''
 
 class Solution:
     def fourSumCount(self, A: List[int], B: List[int], C: List[int], D: List[int]) -> int:
