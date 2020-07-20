06166409f282ca447d4110900e3656f0e2cb7f22
SudhaAmarnath <sudha04.a@gmail.com>
2020-07-20 12:46:14 -0700

    first three largest'

--- AlgoExpert/findfirst3largest.py
+++ AlgoExpert/findfirst3largest.py
@@ -1,7 +1,21 @@
 #to find the first 3 largest elements in a list
-from heapq import nlargest
 
+from heapq import nlargest
+def findThreeLargestNumbers(array):
+    # Write your code here.
+	return sorted(nlargest(3,array))
+
+
+#or
+from heapq import nlargest
 def largest(numbers):
     numbers = set(numbers)
     return nlargest(3,numbers)
 print(largest([1,2,3,14,5,13,1]))
+
+#or
+def findThreeLargestNumbers(array):
+    # Write your code here.
+	# return sorted( [x for i,x in enumerate(array)], reverse=True )[:3]
+    lst = sorted( [x for i,x in enumerate(array)], reverse=True )[:3]
+	return sorted(lst)
