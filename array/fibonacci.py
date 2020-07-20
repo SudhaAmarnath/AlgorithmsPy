686e69d4d6787cfec448138f73965f2366fa4a31
SudhaAmarnath <sudha04.a@gmail.com>
2020-07-20 12:56:35 -0700

    fibonacci series

---
+++ array/fibonacci.py
@@ -0,0 +1,63 @@
+#from functools import lru_cache
+#@lru_cache(maxsize=1000)
+fibcache = {}
+def fibonacci(n):
+    if type(n)!= int:
+        raise TypeError("int")
+    if n < 1:
+        raise ValueError("+ve int")
+
+    if n in fibcache:
+        return fibcache[n]
+
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 1
+    elif n > 2:
+        val = fibonacci(n-1)+fibonacci(n-2)
+
+    fibcache[n] = val
+    return val
+
+for i in range(1,20):
+    print(fibonacci(i))
+
+
+'''
+Sample Input
+
+5
+Sample Output
+
+[0, 1, 1, 8, 27]
+Explanation
+
+The first  fibonacci numbers are [0,1,1,2,3], and their cubes are [0, 1, 1, 8, 27].
+'''
+cube = lambda x: pow(x, 3)  # x**3
+
+
+def fibonacci(n):
+    lst = [0, 1]
+    for i in range(2, n):
+        lst.append(lst[i - 2] + lst[i - 1])
+    return (lst[0:n])
+    # return lst
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print(list(map(cube, fibonacci(n))))
+
+#or
+
+def fib(n):
+    #cube  = lambda x: pow(x,3)
+    lst = [1,1]
+    for i in range(2,n):
+        lst.append(lst[i-2]+lst[i-1])
+    return lst
+
+print(fib(3)) #[1, 1, 2]
+print(list(map(lambda x: pow(x, 3),fib(5)))) #[1, 1, 8, 27, 125]
