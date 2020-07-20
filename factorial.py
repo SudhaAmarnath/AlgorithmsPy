12cc959acf19e33fdd9f767163d4a471244a082a
SudhaAmarnath <sudha04.a@gmail.com>
2020-07-20 12:44:39 -0700

    factorial

--- factorial.py
+++ factorial.py
@@ -3,3 +3,13 @@ def factorial(n):
         return 1
     return n * factorial(n-1)
 print(factorial(7))
+
+#or non recursive
+def factorial(n):
+    res = 1
+    for i in range(1,n+1):
+        if i < n:
+            res = res*(i+1)
+    return res
+
+print(factorial(3)) #6
