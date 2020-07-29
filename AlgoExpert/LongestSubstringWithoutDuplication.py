2df3a9baf9e276cd0fc557bc27fae52eeb41e4c0
SudhaAmarnath <sudha04.a@gmail.com>
2020-07-28 18:04:43 -0700

    longest without repetation

---
+++ AlgoExpert/LongestSubstringWithoutDuplication.py
@@ -0,0 +1,13 @@
+#O(n) time | O(min(n, a)) space #a for repeating char
+def longestSubstringWithoutDuplication(string):
+    # Write your code here.
+    lastseen = {}
+	longest = [0, 1]
+	startidx = 0
+	for i,char in enumerate(string):
+		if char in lastseen:
+			startidx = max(startidx, lastseen[char] + 1)
+		if longest[1] - longest[0] < i + 1 - startidx: #to update duplicte, to compare longest
+			longest = [startidx, i+1]
+		lastseen[char] = i
+	return string[longest[0]:longest[1]]
