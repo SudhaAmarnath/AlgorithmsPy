102245056d873fe6cba9e61be5a8c03cc5b4a9f2
SudhaAmarnath <42689991+SudhaAmarnath@users.noreply.github.com>
2020-08-15 12:44:17 -0700

    Create basicstats.py

---
+++ HakerRankAI/basicstats.py
@@ -0,0 +1,19 @@
+#https://www.hackerrank.com/challenges/stat-warmup/problem
+
+import numpy as np
+from scipy import stats
+
+n = int(input())
+a = np.array(input().split())
+a = a.astype(np.int32)
+
+mean = np.mean(a)
+sigma = np.std(a)
+medi = np.median(a)
+mod = stats.mode(a)
+ci = stats.norm.interval(0.950004, loc=mean, scale=sigma/np.sqrt(n))
+print("%.1f"%mean)
+print("%.1f"%medi)
+print("%.0f"%mod[0][0])
+print("%.1f"%sigma)
+print("%.1f %.1f"%(ci[0],ci[1]))
