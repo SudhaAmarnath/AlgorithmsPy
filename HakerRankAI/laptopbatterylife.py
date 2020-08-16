49485375e6a1dcc25b5177c077a334c6310a6d4a
SudhaAmarnath <42689991+SudhaAmarnath@users.noreply.github.com>
2020-08-16 14:52:55 -0700

    Update laptopbatterylife.py

--- HakerRankAI/laptopbatterylife.py
+++ HakerRankAI/laptopbatterylife.py
@@ -38,7 +38,38 @@ if __name__ == '__main__':
         print(round(result[0][0],2))
     else:
         print(8.00)
+ 
 
+#solution 2:
+'''
+import re
+import sys
+
+if __name__ == '__main__':
+    timeCharged = float(input())
+
+    if timeCharged >= 4:
+        result=8
+    else:
+        import pandas as pd
+        logs = pd.read_csv('trainingdata.txt', header=None)
+        logs.columns = ['charging', 'bat']
+        logs2 = logs[logs.charging < 4]
+
+        from sklearn.linear_model import LinearRegression
+        from sklearn.model_selection import train_test_split
+        import numpy as np
+        x = logs2.charging.values.reshape(-1,1) #one feature
+        y = logs2.bat
+        xtrain, xtest, ytrain, ytest = train_test_split(x,y,random_state=1)
+        lr = LinearRegression()
+        lr.fit(xtrain, ytrain)
+
+        result=float(lr.predict(np.array([timeCharged]).reshape((-1, 1))))
+
+    print(result)
+
+'''
 '''
 # Easy after reading the pattern in the input data
 if __name__ == '__main__':
