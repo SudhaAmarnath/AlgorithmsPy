1f25bbd486fce7f76f05c0bfcd96da838794f14e
SudhaAmarnath <42689991+SudhaAmarnath@users.noreply.github.com>
2020-08-16 18:45:54 -0700

    Create missingstockprices.py

---
+++ HakerRankAI/missingstockprices.py
@@ -0,0 +1,21 @@
+# https://www.hackerrank.com/challenges/missing-stock-prices/problem
+
+import pandas as pd
+import numpy as np
+n = int(input())
+data = []
+for _ in range(n):
+    data.append(input().split())
+
+data = pd.DataFrame(data)
+data.columns = ['date', 'time', 'price']
+data.price = pd.to_numeric(data.price, errors='coerce')
+del data['date']
+del data['time']
+
+missingidxlist = data.loc[pd.isna(data["price"]), :].index
+data.interpolate(method='polynomial',order=2,inplace=True)
+
+for i in missingidxlist:
+    print(-1e32)
+    #print(data.at[i, 'price'])
