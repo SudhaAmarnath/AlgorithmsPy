4e57e0566f61c041907a3caa85cce511560424a6
SudhaAmarnath <sudha04.a@gmail.com>
2020-08-25 20:56:52 -0700

    adding solution2

--- strings/goatlatin.py
+++ strings/goatlatin.py
@@ -7,6 +7,8 @@ output
 "Imaa peaksmaaa oatGmaaaa atinLmaaaaa"
 '''
 
+#solution 1
+#time O(1)
 class Solution:
     def toGoatLatin(self, S: str) -> str:
         words = S.split()
@@ -18,3 +20,20 @@ class Solution:
                 words[i] = words[i][1:] + words[i][0] + "ma" + "a" * (i + 1)
 
         return " ".join(words)
+
+#solution 2
+#time O(n)
+
+class Solution(object):
+    def toGoatLatin(self, S):
+
+        goatlatin = []
+
+        for i, word in enumerate(S.split()):
+            if word[0].lower() in 'aeiou':
+                goatlatin.append(word + 'ma' + (i + 1) * 'a')
+            else:
+                goatlatin.append(word[1:] + word[0] + 'ma' + (i + 1) * 'a')
+
+        return ' '.join(goatlatin)
+
