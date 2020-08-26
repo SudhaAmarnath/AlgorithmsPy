55c272e6340a5169b3ce53d9cc834d7c1ed2cdd5
SudhaAmarnath <sudha04.a@gmail.com>
2020-08-25 20:02:38 -0700

    code using train/test

--- HakerRankAI/laptopbatterylife.py
+++ HakerRankAI/laptopbatterylife.py
@@ -1,47 +1,10 @@
 #https://www.hackerrank.com/challenges/battery/problem
 
-import sys
-import numpy as np
-from sklearn.linear_model import LinearRegression
+#!/bin/python3
 
-if __name__ == '__main__':
-    timeCharged = float(input().strip())
-
-    X = []
-    y = []
-
-    with open('trainingdata.txt') as file:
-        for line in file:
-            c, h = line.split(',')
-            c = float(c)
-            h = float(h)
-            if h != 8.00:
-                X.append(c)
-                y.append(h)
-
-    X = np.array(X)
-    y = np.array(y)
-
-    X = X.reshape(-1,1)
-    y = y.reshape(-1,1)
-
-    regressor = LinearRegression()
-    regressor.fit(X,y)
-
-    testset = []
-    testset.append(timeCharged)
-    testset = np.array(testset)
-    testset = testset.reshape(1,-1)
-    result = regressor.predict(testset)
-
-    if testset[0] <= 4:
-        print(round(result[0][0],2))
-    else:
-        print(8.00)
- 
-
-#solution 2:
-'''
+import math
+import os
+import random
 import re
 import sys
 
@@ -68,14 +31,3 @@ if __name__ == '__main__':
         result=float(lr.predict(np.array([timeCharged]).reshape((-1, 1))))
 
     print(result)
-
-'''
-'''
-# Easy after reading the pattern in the input data
-if __name__ == '__main__':
-    timeCharged = float(input().strip())
-    if timeCharged >= 4:
-        print(8.0)
-    else:
-        print(timeCharged*2)
-'''
