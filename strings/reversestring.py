79ab44b14e46fac9e1cdadb8afe927b73bc4f830
SudhaAmarnath <42689991+SudhaAmarnath@users.noreply.github.com>
2020-11-20 17:11:55 -0800

    Update reversestring.py

--- strings/reversestring.py
+++ strings/reversestring.py
@@ -17,3 +17,23 @@ print(''.join(reversalstring().reverse(a)))
 s = 'string'
 s = list(s)
 print(''.join(s[::-1]))
+
+-------------------------------------------
+def reverse(a):
+    k = ""
+    for i in a:
+        k = i + k
+    return k
+print(reverse("sudha"))
+
+---------------------------------------------
+def reverse(a):
+    output_len = len(a)
+    output = [None] * output_len
+    output_last = output_len - 1
+    for i in a:
+        output[output_last] = i
+        output_last -= 1
+    return ''.join(output)
+
+print(reverse("sudha"))
